ad04b7ec87edaab2cfcb6e5553b858d14c43b778
fahimulkabirtest <fahimulkabirunt@gmail.com>
2026-06-21 17:09:52 -0500

    Update site.ts

--- src/types/site.ts
+++ src/types/site.ts
@@ -4,4 +4,12 @@ export type SiteSettings = {
   yourFirstName: string;
   yourLastName: string;
   navbarTitle?: string;
+  allowIndexing?: boolean;
+  disallowAdmin?: boolean;
+  // Navigation Toggles
+  showHome: boolean;
+  showPublications: boolean;
+  showResearch: boolean;
+  showMembers: boolean;
+  showJoinUs: boolean;
 };

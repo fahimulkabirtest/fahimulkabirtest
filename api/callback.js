f599dd398b61b603b2943ce7798486dfeb3f8f15
fahimulkabirtest <fahimulkabirunt@gmail.com>
2026-06-21 02:37:28 -0500

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,17 +1,3 @@
 import { createVercelCompleteHandler } from "netlify-cms-oauth-provider-node";
 
-export default function handler(req, res) {
-  const host = req.headers.host;
-  const protocol = host.includes("localhost") ? "http" : "https";
-  const origin = `${protocol}://${host}`;
-
-  const config = {
-    clientId: process.env.OAUTH_CLIENT_ID,
-    clientSecret: process.env.OAUTH_CLIENT_SECRET,
-    completeUrl: `${origin}/api/callback`,
-    origin: origin, // This automatically satisfies the security check!
-  };
-
-  const completeHandler = createVercelCompleteHandler(config);
-  return completeHandler(req, res);
-}
+export default createVercelCompleteHandler({}, { useEnv: true });
